d572885cd31cb3ee1dea1d88ff106a945c2bd8fd
Yura Zatsepin <yura.zatsepin@gmail.com>
2012-10-18 14:27:40 +0600

    some fixes

--- javascript/application.js
+++ javascript/application.js
@@ -1,18 +1,2 @@
 jQuery(document).ready(function($){
-
-  $('#github').mouseover(function(){
-    $('#mousing').text("GitHub");
-  });
-
-  $('#github').mouseout(function(){
-    $('#mousing').html("&nbsp;");
-  });
-
-  $('#twitter').mouseover(function(){
-    $('#mousing').text("Twitter");
-  });
-
-  $('#twitter').mouseout(function(){
-    $('#mousing').html("&nbsp;");
-  });
 });
